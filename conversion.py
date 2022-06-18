3ba445fa6d9455f3e953bae6545becf1a5ece20a
AswinGopakumar <aswingopakumar04@gmail.com>
2022-06-18 20:53:35 +0530

    fix function name

--- conversion.py
+++ conversion.py
@@ -1,19 +1,15 @@
 from multiprocessing.context import assert_spawning
 from turtle import clear
 
-#function to convert input string to ascii
-def stringtoascii(s):
-       
+#function to convert input string to ascii and then to binary
+def stringtobin(s):
+        #converting string to ascii
         alist = [ord(c) for c in s] 
         print(alist)
+        #converting ascii to binary
         ltob = [ bin(list_item)[2:] for list_item in alist ]
         print(ltob)
 
 
-
-#function to convert ascii to binary
-def asciitobin():
-        pass
-                
 s = input("Enter your secret messege: ")
-stringtoascii(s)
+stringtoascii(s) 
