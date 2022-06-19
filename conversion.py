e9c2515b8b2ed3f8a121abb437024c0718021790
AswinGopakumar <aswingopakumar04@gmail.com>
2022-06-19 10:25:56 +0530

    convert to binary string

--- conversion.py
+++ conversion.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 from multiprocessing.context import assert_spawning
 from turtle import clear
 
@@ -6,10 +7,17 @@ def stringtobin(s):
         #converting string to ascii
         alist = [ord(c) for c in s] 
         print(alist)
-        #converting ascii to binary
+        #converting ascii to binary string
+        binstr = ""
         ltob = [ bin(list_item)[2:] for list_item in alist ]
-        print(ltob)
+        for list_item in ltob:
+                if len(list_item)%2 != 0:
+                        list_item = "0"+list_item
+                        binstr = binstr + list_item
+        print(binstr)
 
 
 s = input("Enter your secret messege: ")
-stringtoascii(s) 
+stringtobin(s)
+
+
