874c7c1bd583724c5f979dcb8b705fe36ec708d3
AswinGopakumar <aswingopakumar04@gmail.com>
2022-06-19 12:07:49 +0530

    convert to dna and to corresponding ascii

--- conversion.py
+++ conversion.py
@@ -15,9 +15,33 @@ def stringtobin(s):
                         list_item = "0"+list_item
                         binstr = binstr + list_item
         print(binstr)
+        binToDna(binstr)
 
+#function to convert binary string to DNA bases
+def binToDna(bstr):
+        outputStr = ''
+        for start in range(0, len(bstr), 2):
+                word = bstr[start:start+2]
+                if word == '00': outputStr += 'A'
+                elif word == '01': outputStr += 'T'
+                elif word == '10': outputStr += 'C'
+                elif word == '11': outputStr += 'G'
+        print(outputStr)
+        dnaToAscii(outputStr)
+
+#converts dna to ascii list and multiply it with a factor
+def dnaToAscii(dstr):
+        factor = 7
+        f_list = list()
+        da_list = [ord(c) for c in dstr]
+        print(da_list)
+        for a in da_list:
+                a = a*factor
+                f_list.append(a)
+        print(f_list)
 
 s = input("Enter your secret messege: ")
 stringtobin(s)
+
 
 
