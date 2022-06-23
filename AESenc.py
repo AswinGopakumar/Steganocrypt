efa61164f62365f681291205c644761fb67be8bc
AswinGopakumar <aswingopakumar04@gmail.com>
2022-06-23 20:00:39 +0530

    add return statements to functions

--- AESenc.py
+++ AESenc.py
@@ -1,27 +1,37 @@
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
+import sys
+
+
 def encryption():
-    
-    key_location = "E:\\my_key.bin" # A safe place to store a key. Can be on a USB or even locally on the machine (not recommended unless it has been further encrypted)
+    # A safe place to store a key. Can be on a USB or even locally on the machine (not recommended unless it has been further encrypted)
+    key_location = "E:\\my_key.bin"
 
     # Generate the key
     key = get_random_bytes(32)
 
     # Save the key to a file
-    file_out = open(key_location, "wb") # wb = write bytes
-    file_out.write(key)
-    file_out.close()
+    try:
+        file_out = open(key_location, "wb")  # wb = write bytes
+        file_out.write(key)
+        file_out.close()
+    except FileNotFoundError:
+        print("Insert the usb drive")
+        print("Encryption failed")
+        sys.exit(1)
 
-    #AES-CFB mode encryption
+    # AES-CFB mode encryption
     output_file = 'encrypted.bin'
-    data = open('cipher.txt','rb')
+    data = open('cipher.txt', 'rb')
     data_from_file = data.read()
     data.close
 
-    cipher = AES.new(key, AES.MODE_CFB) # CFB mode
-    ciphered_data = cipher.encrypt(data_from_file) # Only need to encrypt the data, no padding required for this mode
+    cipher = AES.new(key, AES.MODE_CFB)  # CFB mode
+    # Only need to encrypt the data, no padding required for this mode
+    ciphered_data = cipher.encrypt(data_from_file)
 
     file_out = open(output_file, "wb")
     file_out.write(cipher.iv)
     file_out.write(ciphered_data)
     file_out.close()
+    return None
--- conversion.py
+++ conversion.py
@@ -4,62 +4,79 @@ from multiprocessing.context import assert_spawning
 from turtle import clear
 import gzip
 import AESenc
-#function to convert input string to ascii and then to binary
+# function to convert input string to ascii and then to binary
+
+
 def stringtobin(s):
-        #converting string to ascii list
-        alist = [ord(c) for c in s] 
-        print(alist)
-        #converting ascii to binary string
-        binstr = ""
-        ltob = [ bin(list_item)[2:] for list_item in alist ]
-        for list_item in ltob:
-                if len(list_item)%2 != 0:
-                        list_item = "0"+list_item
-                        binstr = binstr + list_item
-        print(binstr)
-        binToDna(binstr)
+    # converting string to ascii list
+    alist = [ord(c) for c in s]
+    print(alist)
+    # converting ascii to binary string
+    binstr = ""
+    ltob = [bin(list_item)[2:] for list_item in alist]
+    for list_item in ltob:
+        if len(list_item) % 2 != 0:
+            list_item = "0"+list_item
+            binstr = binstr + list_item
+    print("binary string :", binstr)
+    return binstr
 
-#function to convert binary string to DNA bases
+# function to convert binary string to DNA bases
+
+
 def binToDna(bstr):
-        outputStr = ''
-        for start in range(0, len(bstr), 2):
-                word = bstr[start:start+2]
-                if word == '00': outputStr += 'A'
-                elif word == '01': outputStr += 'T'
-                elif word == '10': outputStr += 'C'
-                elif word == '11': outputStr += 'G'
-        print(outputStr)
-        dnaToAscii(outputStr)
+    outputStr = ''
+    for start in range(0, len(bstr), 2):
+        word = bstr[start:start+2]
+        if word == '00':
+            outputStr += 'A'
+        elif word == '01':
+            outputStr += 'T'
+        elif word == '10':
+            outputStr += 'C'
+        elif word == '11':
+            outputStr += 'G'
+    print("DNA bases :", outputStr)
+    return outputStr
 
-#converts dna to ascii list and multiply it with a factor
+# converts dna to ascii list and multiply it with a factor
+
+
 def dnaToAscii(dstr):
-        factor = 7
-        f_list = list()
-        da_list = [ord(c) for c in dstr]
-        print(da_list)
-        for a in da_list:
-                a = a*factor
-                f_list.append(a)
-        f_joined = " ".join(str(n) for n in f_list)
-        print(f_joined)
-        compression(f_joined)
+    factor = 7
+    f_list = list()
+    da_list = [ord(c) for c in dstr]
+    print(da_list)
+    for a in da_list:
+        a = a*factor
+        f_list.append(a)
+    f_joined = " ".join(str(n) for n in f_list)
+    print("dna to ascii :", f_joined)
+    return f_joined
 
-#writes the ciphertext to a text file
+# writes the ciphertext to a text file
+
+
 def fwrite(dnaascii):
-        with open("cipher.txt",'wb') as f:
-                f.write(dnaascii)
-                f.close()
+    with open("cipher.txt", 'wb') as f:
+        f.write(dnaascii)
+        f.close()
+    print("fwrite")
+    return None
+# compresses the message using gzip
 
-#compresses the message using gzip
+
 def compression(ascString):
-        compressed_value = gzip.compress(bytes(ascString, 'utf-8'))
-        print(compressed_value)
-        fwrite(compressed_value)
+    compressed_value = gzip.compress(bytes(ascString, 'utf-8'))
+    print("compression :", compressed_value)
+    return compressed_value
+
 
 s = input("Enter your secret messege: ")
-stringtobin(s)
+bistr = stringtobin(s)
+btd = binToDna(bistr)
+dToA = dnaToAscii(btd)
+compr = compression(dToA)
+filewrite = fwrite(compr)
+
 AESenc.encryption()
-
-
-
-
